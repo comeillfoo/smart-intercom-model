b12295736c1d7645357120d05032c425a8c734b4
come_ill_foo <rayveleaveme34@inbox.ru>
2024-03-25 23:15:10 +0000

    feat(camera): working tcp frame size negotiator

--- camera/camera-n-lock.py
+++ camera/camera-n-lock.py
@@ -1,27 +1,95 @@
 #!/usr/bin/env python3
 import sys
+import argparse
+import socket
+import pickle
+import struct
 import cv2
+from contextlib import closing
 
-WINDOW_NAME='cam1'
+
+SUPPORTED_PROTOS = [ 'tcp', 'udp' ] # TODO: rtsp
+
+PROTOS_MAP = {
+    'tcp': socket.IPPROTO_TCP,
+    'udp': socket.IPPROTO_UDP,
+}
+
+
+def argparser() -> argparse.ArgumentParser:
+    p = argparse.ArgumentParser('camera', description='''
+    Runs camera that connects to SBC and sends frames from WebCam for receiving
+    the decision about door unlocking.
+    ''')
+
+    default_proto = 'tcp'
+    p.add_argument('-p', '--proto', choices=SUPPORTED_PROTOS, default=default_proto,
+        help=f'belect the transfer protocol, default \'{default_proto}\'')
+
+    p.add_argument('-P', '--port', type=int, required=True, help='connect port')
+
+    default_host = 'localhost'
+    p.add_argument('host', nargs='?', default=default_host,
+                   help=f'connect address, default \'{default_host}\'')
+    return p
+
+
+def tcp_negotiate(cam, sk: socket.socket) -> int:
+    test_ret, test_frame = cam.read()
+    if not test_ret: return 1
+    sk.sendall(struct.pack('NNN', *test_frame.shape))
+    return 0
+
+
+def handle_kbd_int(client):
+    def wrapper(cam, sk: socket.socket, sockaddr) -> int:
+        try:
+            return client(cam, sk, sockaddr)
+        except KeyboardInterrupt:
+            print('Stopping client...')
+        return 0
+    return wrapper
+
+
+@handle_kbd_int
+def tcp_client(cam, sk: socket.socket, sockaddr) -> int:
+    sk.connect(sockaddr)
+    ret = tcp_negotiate(cam, sk)
+    if ret: return ret
+    while True:
+        pass
+        # camret, frame = cam.read()
+        # if not camret:
+        #     print('failed to grab frame')
+        #     ret = 1
+        #     break
+        # data = pickle.dumps(frame)
+        # sk.sendall(struct.pack('H', len(data)) + data)
+
+    return ret
+
+
+def udp_client(cam, sk: socket.socket, sockaddr) -> int:
+    return 0
+
+
+SERVER_HOOKS = {
+    socket.IPPROTO_TCP: tcp_client,
+    socket.IPPROTO_UDP: udp_client,
+}
+
 
 def main() -> int:
     ret = 0
+    args = argparser().parse_args()
+
     cam = cv2.VideoCapture(0)
-    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_AUTOSIZE)
-    while True:
-        camret, frame = cam.read()
-        if not camret:
-            print('failed to grab frame')
-            ret = 1
-            break
 
-        cv2.imshow(WINDOW_NAME, frame)
-        k = cv2.waitKey(1)
-        if k % 256 == 27: # ESC
-            print('ESC pressed, exitting')
-            break
+    family, type, proto, _, sockaddr = socket.getaddrinfo(args.host, args.port,
+        proto=PROTOS_MAP.get(args.proto, socket.IPPROTO_TCP))[0]
+    with closing(socket.socket(family, type, proto)) as sk:
+        ret = SERVER_HOOKS[proto](cam, sk, sockaddr)
 
-    cv2.destroyAllWindows()
     cam.release()
     return ret
 
